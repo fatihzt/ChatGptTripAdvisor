44cbd8de8094408d1b5dafaaf2d79ee19bee6c03
Muhammet Fatih Özata <104263777+fatihzt@users.noreply.github.com>
2023-03-05 20:04:54 +0300

    geodb fetch api

--- ChatGptBot.Api/Controllers/ChatHistroyController.cs
+++ ChatGptBot.Api/Controllers/ChatHistroyController.cs
@@ -1,8 +1,15 @@
-﻿using ChatGptBot.Business.Service;
+﻿using ChatGptBot.Business.Manager;
+using ChatGptBot.Business.Service;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
 using OpenAI.GPT3;
+using OpenAI.GPT3.Interfaces;
 using OpenAI.GPT3.Managers;
+using OpenAI.GPT3.ObjectModels;
+using System;
 
 namespace ChatGptBot.Api.Controllers
 {
@@ -12,24 +19,37 @@ namespace ChatGptBot.Api.Controllers
     {
         private readonly IChatHistoryService _chatHistoryService;
         private readonly HttpClient _client;
-        private readonly OpenAIService _openAIService;
-        public ChatHistroyController(IChatHistoryService chatHistoryService,HttpClient client, OpenAIService openAIService)
+        private readonly IOpenAIService _openAIService;
+        
+        public ChatHistroyController(IChatHistoryService chatHistoryService,IOpenAIService openAIService,HttpClient client)
         {
             _chatHistoryService = chatHistoryService;
             _client = client;
-            _openAIService = openAIService;
+            this._openAIService = openAIService;
+        }
+        [HttpGet("GetCityName")]
+        public async Task<IActionResult> GetCityName()
+        {
+            string apikey = "1cd672a228msh23352867f930791p170d3ajsn138709139db9";
+            CitiesManager citiesManager = new CitiesManager(apikey);
+            string[] cityName = await citiesManager.GetCityNames();
+            return Ok(cityName);
         }
 
         [HttpPost]
         public async Task<IActionResult> Create([FromQuery] string prompt)
         {
+            
+
             var result=await _openAIService.Completions.CreateCompletion(new()
             {
-                Prompt = prompt,
+                Prompt = $"{prompt} için yol tarifi verir misin ?",
                 MaxTokens = 500
 
-            });
-            return Ok(result);
+            },Models.TextDavinciV3);
+            return Ok(result.Choices[0].Text);
         }
+        
+
     }
 }
